a55edb003abd5de8088c67517612e881b1abdd86
JakubSzukala <burski09@outlook.com>
2024-08-24 11:45:58 +0200

    Add double jump

--- player/src/player.gd
+++ player/src/player.gd
@@ -5,15 +5,21 @@ const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+var jump_counter: int = 2
 
 func _physics_process(delta):
+	# Apply gravity
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	# Handle double jump
+	if Input.is_action_just_pressed("jump") and jump_counter > 1:
 		velocity.y = JUMP_VELOCITY
+		jump_counter -= 1
+	if is_on_floor():
+		jump_counter = 2
 
+	# Handle horizontal movement
 	var direction = Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
