6d3da7e41be11fe671b61dedb1deaff2fbeb6579
JakubSzukala <burski09@outlook.com>
2024-08-24 12:51:45 +0200

    Add different ascending/descending gravities

--- player/src/player.gd
+++ player/src/player.gd
@@ -1,28 +1,34 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@export var speed = 300.0
+@export var jump_velocity = -500.0
+@export var ascend_gravity = 1100
+@export var descend_gravity = 2000
 
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+#var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var jump_counter: int = 2
+
 
 func _physics_process(delta):
 	# Apply gravity
 	if not is_on_floor():
-		velocity.y += gravity * delta
+		if velocity.y > 0:
+			velocity.y += descend_gravity * delta
+		else:
+			velocity.y += ascend_gravity * delta
 
 	# Handle double jump
 	if Input.is_action_just_pressed("jump") and jump_counter > 0:
-		velocity.y = JUMP_VELOCITY
+		velocity.y = jump_velocity
 		jump_counter -= 1
 
 	# Handle horizontal movement
 	var direction = Input.get_axis("left", "right")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = direction * speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, speed)
 
 	move_and_slide()
 
