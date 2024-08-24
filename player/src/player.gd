f1824500a527d72b280fedeba66ebe6676228ac4
JakubSzukala <burski09@outlook.com>
2024-08-24 12:05:29 +0200

    Fix is_on_floor issue

--- player/src/player.gd
+++ player/src/player.gd
@@ -13,11 +13,9 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 
 	# Handle double jump
-	if Input.is_action_just_pressed("jump") and jump_counter > 1:
+	if Input.is_action_just_pressed("jump") and jump_counter > 0:
 		velocity.y = JUMP_VELOCITY
 		jump_counter -= 1
-	if is_on_floor():
-		jump_counter = 2
 
 	# Handle horizontal movement
 	var direction = Input.get_axis("left", "right")
@@ -27,3 +25,6 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+	if is_on_floor():
+		jump_counter = 2
