33af414f6183b5fe9710dbe49b24d043b1dc0b59
Szymon Kwiatkowski <szymon.z.kwiatkowski@gmail.com>
2024-08-24 20:55:29 +0200

    feat: lvls cleanup

--- ui/src/menu.gd
+++ ui/src/menu.gd
@@ -16,7 +16,7 @@ func _process(delta):
 
 
 func _on_play_pressed():
-	get_tree().change_scene_to_file("res://level_1.tscn")
+	get_tree().change_scene_to_file("res://level/scn/level_base.tscn")
 
 
 func _on_quit_pressed():
