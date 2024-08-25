77eab3e588a797dc50db5771c37641d5d28ee1d8
Miaurycy Kuwuawski <46225267+Gladiu@users.noreply.github.com>
2024-08-25 13:31:04 +0200

    Signed-off-by: Gladi <maurycyk1@tlen.pl> (#21)

--- level/scn/level.gd
+++ level/scn/level.gd
@@ -3,12 +3,10 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$Sun.pivot_point = $Map.get_pivot_pos()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
+func _process(delta):
+	
 	pass
-
-func get_pivot_pos():
-	return $PivotPoint.global_position
