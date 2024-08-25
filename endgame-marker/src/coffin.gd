44090aa0e3dc2ec50bc126f259b070d257e005c4
Jakub Szukała <40769639+JakubSzukala@users.noreply.github.com>
2024-08-25 14:52:27 +0200

    Add damage (#25)
    * Add game win splash
    
    * Wire up game end marked to level switching logic
    
    * Add damage taking from sun

--- endgame-marker/src/coffin.gd
+++ endgame-marker/src/coffin.gd
@@ -1,6 +1,15 @@
 extends Node2D
 
 
+var end_level_request: bool = false
+
+
 func _on_body_entered(body):
-	print("Game over?")
-	pass
+	if body as Player:
+		end_level_request = true
+
+
+func _process(_delta):
+	if end_level_request == true:
+		end_level_request = false
+		get_tree().change_scene_to_file("res://ui/scn/game_win_screen.tscn")
--- level/scn/level.gd
+++ level/scn/level.gd
@@ -3,7 +3,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$Sun.pivot_point = $Map.get_pivot_pos()
+	$Player.sun_area = $Sun/Area2D
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
